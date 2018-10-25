84c67ee4c905f51891124cb0c4d52e0b7ca46727
jjohn <jjohn@virtusa.com>
2018-10-25 12:22:33 +0100

    Remove es6 again

--- express-server.js
+++ express-server.js
@@ -7,4 +7,6 @@ app.get('/', function(req, res) {
     res.send('Hello World!!!!');
 } );
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, function(){
+    console.log(`Example app listening on port ${port}!`);
+})
